8b9d08d49a75ccff6409c62158723fc9a6f5e363
danilkberlin <danilkravets19@gmail.com>
2024-01-04 11:31:39 +0100

    мы создвли функцию toggleActive

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ function taggleOpen(){
     this.classList.taggle('open');
 }
 
+function toggleActive(e){
+    console.log(e.propertyName)
+    if (e.propertyName.includes('flex')){
+        this.classList.taggle('open-active')
+    }
+}
+
 penals.forEach(penal => penal.addEventListener('click', taggleOpen))
 penals.forEach(penal => penal.addEventListener('transitionend', toggleActive))
